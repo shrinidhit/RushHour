44dbc9d7b63b1f2e26aae16fdd4bb1cbc01aa220
Allison Patterson <allison.patterson@students.olin.edu>
2014-09-15 21:57:39 -0400

    Update validate_move.

--- rush.py
+++ rush.py
@@ -14,7 +14,7 @@ import numpy
 
 #Global Variables:
 GRID_SIZE = 6
-level1 = "A21dB31rC51dD61dE42dF63dI34rH45dX23r"
+level1 = "A21dB31rC51dD61dE42dF63dI34rH45dX23r" # initial coordinates of each block object (yikes!)
 
 #Classes
 
@@ -88,13 +88,31 @@ def fail (msg):
     raise StandardError(msg)
 
 
-def validate_move (brd,move):
+def validate_move (brd,move): ##### Aaaaaaahhhhhhh... #####
     # FIX ME!
+    row_new, column_new = block.coordinate + block.move ##### I may have made this one up #####
     # check that piece is on the board
-    # check that piece placed so it can move in that direction
+    if block.name not in board.blocks:
+        fail ("Block named does not exist on board")
     # check that piece would be in bound
-    # check that path to target position is free
-    return False
+    elif row_new < 0:
+        fail ("Block can't go past the top edge of the board.")
+    elif row_new > 5:
+        fail ("Block can't go past the bottom edge of the board.")
+    elif column_new < 0:
+        fail ("Block can't go past the left edge of the board.")
+    elif column_new > 5:
+        fail ("Block can't go past the right edge of the board.")
+    # check that piece placed so it can move in that direction
+    elif block.direction != "r" or "d":
+        fail ("Selected block cannot move in that direction.")
+    # check that path to target position is free    
+    elif row_new != 0:
+        fail ("There is another block preventing that move.")
+    elif column_new != 0:
+        fail ("There is another block preventing that move.")
+    else:
+        return True
 
 
 def read_player_input (brd):
@@ -123,7 +141,7 @@ def print_board (brd):
             else:
                 print car.name,
         print ""
-    print ""
+    print "" ##### do we need this line? #####
 
 
     
