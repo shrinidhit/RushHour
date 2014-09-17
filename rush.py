0150a16ebd3f710d070064f2f51d231342748b28
Shrinidhi Thirumalai <shrinidhit@Shrinidhi.(none)>
2014-09-17 15:53:47 -0400

    Finishing part 1

--- rush.py
+++ rush.py
@@ -46,13 +46,22 @@ class board(object):
         #     row.append(0)
         # for i in range(0, GRID_SIZE):
         #     self.grid.append(row)
+    def add_block(self, block):
+        # add new block to board.blocks list
+        self.blocks.append(block)
+        self.blocknames.append(block.name)
+        self.update_grid(block)
+    def grid_object(self, row, column):
+        return self.grid[row - 1][column -1]
+    def grid_assign(self, row, colunm, newitem):
+        self.grid[row - 1][colunm - 1] = newitem
 
     def update_grid(self, block):
 
         #Getting block position
         row_start, column_start = block.coordinate
-        row_start = row_start - 1;
-        column_start = column_start - 1
+        row_start = row_start
+        column_start = column_start
 
         if block.direction == "r":
             row_end = row_start + block.size
@@ -64,23 +73,18 @@ class board(object):
             fail ("Invalid Block direction. Valid inputs: r for ""right"" and d for ""down""")
 
         #Deleting block from old grid:
-        for i in range(0, self.size):
+        for i in range(1, self.size + 1):
             while block in self.grid[i]:
-                loc = self.grid[i].index(block)
-                self.grid[i][loc] = 0
+                loc = self.grid[i].index(block) + 1
+                self.brd.grid_assign(i, loc, 0)
 
         #Putting block in new position:
         for row in range(row_start, row_end):
             for column in range(column_start, column_end):
-                if self.grid[column][row] == 0:
-                    self.grid[column][row] = block
+                if self.grid_object(row,column) == 0:
+                    self.grid_assign(row,column, block)
                 else:
                     fail ("Blocks can not overlap")
-    def add_block(self, block):
-        # add new block to board.blocks list
-        self.blocks.append(block)
-        self.blocknames.append(block.name)
-        self.update_grid(block)
 
 
 # fail somewhat gracefully
@@ -113,13 +117,20 @@ def create_initial_level (level_string):
         start_board.add_block(string_to_new_object(object_string))
     return start_board
 
+def get_player_input():
+    move=raw_input('Enter your move(example: Au2) :')
+    return move
+
 def read_player_input (brd, move):
     blockname = move[0]
     direction = move[1]
-    amount = move[2]
+    amount = int(move[2])
 
     #Checks if block is on board
     if blockname in brd.blocknames:
+        block = name_to_object(brd, blockname)
+    elif blockname.upper() in brd.blocknames:
+        blockname = blockname.upper()
         block = name_to_object(brd, blockname)
     else: fail ("Block is not on board")
 
@@ -131,33 +142,33 @@ def read_player_input (brd, move):
 
     #Gets new coordinates
     currentx, currenty = block.coordinate
+    print block.coordinate
     if direction == 'u': # up
-        coordinate_new = (currentx, currenty + int(amount))
-    elif direction == 'd': # down
         coordinate_new = (currentx, currenty - int(amount))
+    elif direction == 'd': # down
+        coordinate_new = (currentx, currenty + int(amount))
     elif direction == 'l': # left
-        coordinate_new = (currentx + int(amount), currenty)
-    elif direction == 'r': # right
         coordinate_new = (currentx - int(amount), currenty)
+    elif direction == 'r': # right
+        coordinate_new = (currentx + int(amount), currenty)
 
     #Checks if new coordinate is within boundries
-    if 0 >= coordinate_new[0] or coordinate_new[0] >=5:
+    if 1 > coordinate_new[0] or coordinate_new[0] > 6:
         fail ("Move not in bounds")
 
     #Checks if path to new coordinate is free:
     for x in range(currentx, coordinate_new[0] + 1):
         for y in range(currenty, coordinate_new[1] + 1):
-            if grid[x][y] !=0:
+            if brd.grid_object(x,y) !=0 and brd.grid_object(x,y) != brd.grid_object(currentx, currenty):
                 fail ("Move is blocked by another piece")
 
-    return [block, coordinate_new]
+    return [blockname, coordinate_new]
 
-def update_board (brd,move):
-    [block, new_coordinate] = read_player_input(move)
-    block.coordinate = new_coordinate
-    
+def update_board (brd, blockname, coordinate_new):
+    block = name_to_object(brd, blockname)
+    block.coordinate = coordinate_new
+    brd.update_grid(block)
     return brd
-
 
 def print_board (brd):
     # FIX ME!
@@ -170,12 +181,17 @@ def print_board (brd):
                 print car.name,
         print ""
     print "" ##### do we need this line? ##### <-----Yup, it adds an empty space after the whole grid is printed.
-
-
     
 def done (brd):
     #Check if object X's coordinate is at end position. Return True if it is
-    return True
+    endpoint1 = brd.grid_object(3,5)
+    endpoint2 = brd.grid_object(3,6)
+
+    if type(endpoint1) is block:
+        if endpoint1.name == 'X': return True
+    elif type(endpoint2) is block:
+        if endpoint2.name == 'X': return True
+    return False
 
 def main ():
 
@@ -184,16 +200,25 @@ def main ():
     print_board(brd)
 
     while not done(brd):
-        move = read_player_input(brd)
+        playerinput = get_player_input()
+        move = read_player_input(brd, playerinput)
         brd = update_board(brd,move)
         print_board(brd)
 
     print 'YOU WIN! (Yay...)\n'
 
 def test ():
-
     brd = create_initial_level(level1)
+
     print_board(brd)
+
+    while not done(brd):
+        moveString = get_player_input()
+        move = read_player_input(brd, moveString)
+        brd = update_board(brd, move[0], move[1])
+        print_board(brd)
+
+    print 'YOU WIN! (Yay...)\n'
 
 if __name__ == '__main__':
     test()
